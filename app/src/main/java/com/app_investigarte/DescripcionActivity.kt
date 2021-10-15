d4fe89f67b60ca2ea86f7a92a32661ce971b19ba
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2021-10-15 16:31:46 -0500

    Listado de artefactos 0.2.2

--- app/src/main/java/com/app_investigarte/DescripcionActivity.kt
+++ app/src/main/java/com/app_investigarte/DescripcionActivity.kt
@@ -2,10 +2,19 @@ package com.app_investigarte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app_investigarte.database.DatabaseAccess
 
 class DescripcionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_descripcion)
+
+
+        val databaseAccess = DatabaseAccess.getInstance(applicationContext)
+        databaseAccess.open()
+        val informacionArtefacto = databaseAccess.getDescription(1);
+        databaseAccess.close()
+
+
     }
 }
