61fae899dffb35281caab6b114963e93bb72f53c
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2021-10-22 17:12:54 -0500

    Cambio de colores app

--- app/src/main/java/com/app_investigarte/RecyVWAdapter.java
+++ app/src/main/java/com/app_investigarte/RecyVWAdapter.java
@@ -1,11 +1,14 @@
 package com.app_investigarte;
 
+import android.content.Context;
+import android.content.Intent;
 import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -16,7 +19,7 @@ import java.util.List;
 public class RecyVWAdapter extends RecyclerView.Adapter<RecyVWAdapter.ViewHolder>
 {
     private List<RecyclerViewModel> ArtifactList;
-
+    Intent intent;
     public RecyVWAdapter(List<RecyclerViewModel>ArtifactList)
     {
         this.ArtifactList=ArtifactList;
@@ -34,10 +37,9 @@ public class RecyVWAdapter extends RecyclerView.Adapter<RecyVWAdapter.ViewHolder
     public void onBindViewHolder(@NonNull RecyVWAdapter.ViewHolder holder, int position)
     {
         int imagen=ArtifactList.get(position).getImageView1();
-
         String name=ArtifactList.get(position).getTexview1();
 
-
+       holder.startDescription(holder.itemView);
         holder.setData(imagen,name);
     }
 
@@ -58,9 +60,18 @@ public class RecyVWAdapter extends RecyclerView.Adapter<RecyVWAdapter.ViewHolder
 
             imageView1=itemView.findViewById(R.id.artifact1_imagvw);
             textView=itemView.findViewById(R.id.artifact1_txtvw);
-
         }
 
+
+        public void startDescription(View view){
+
+            view.setOnClickListener( v -> {
+                Toast.makeText(view.getContext(),"id" + textView.getText(),Toast.LENGTH_SHORT).show();
+                intent=new Intent(view.getContext(),NavDrawerActivity.class);
+                view.getContext().startActivity(intent);
+            });
+
+        }
 
         public void setData(int imagen, String name)
         {
