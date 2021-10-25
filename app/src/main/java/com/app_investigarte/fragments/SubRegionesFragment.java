3945a4f6ae523ca2bed8ac33f016b835a9e64e9b
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2021-10-25 15:18:11 -0500

    creacion del metodo para mostrartodos los artefactos

---
+++ app/src/main/java/com/app_investigarte/fragments/SubRegionesFragment.java
@@ -0,0 +1,29 @@
+package com.app_investigarte.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app_investigarte.R;
+
+public class SubRegionesFragment extends Fragment {
+
+    public SubRegionesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sub_regiones, container, false);
+    }
+}
