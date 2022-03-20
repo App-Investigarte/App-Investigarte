c18747101dd57dc3288cfe133d189a060543ec9f
juan666 <Juanpabloflorezcruz@gmail.com>
2022-03-19 20:45:42 -0500

    Registro V1.0

--- app/src/main/java/com/app_investigarte/LoginActivity2.kt
+++ app/src/main/java/com/app_investigarte/LoginActivity2.kt
@@ -7,16 +7,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import androidx.core.content.ContentProviderCompat.requireContext
 import com.app_investigarte.databinding.ActivityLogin2Binding
+import com.google.android.material.datepicker.MaterialDatePicker
 import kotlinx.android.synthetic.main.activity_login2.*
 
 class LoginActivity2 : AppCompatActivity() {
 
-    private lateinit var binding:ActivityLogin2Binding
+    /*private lateinit var binding:ActivityLogin2Binding*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLogin2Binding.inflate(layoutInflater)
+        setContentView(R.layout.activity_login2)
+
+        //DropdownMenu
+/*        binding = ActivityLogin2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val dias = resources.getStringArray(R.array.dias)
@@ -24,14 +29,23 @@ class LoginActivity2 : AppCompatActivity() {
 
         with(binding.autocompletetxtdias){
             setAdapter(adapter)
-        }
+        }*/
 
         btn_back.setOnClickListener {
             startActivity(Intent(this,LoginActivity::class.java))
         }
-
-
-
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Seleccionar fecha")
+                .build()
+        btn_date.setOnClickListener {
+            datePicker.show(supportFragmentManager, "tag");
+        }
+        datePicker.addOnPositiveButtonClickListener {
+            var date = datePicker.selection.toString()
+            txt_date.setText(date)
+            // Respond to positive button click.
+        }
 
 
     }
