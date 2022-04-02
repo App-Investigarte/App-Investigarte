60ca221c3927c4b6ffec0f2841a10db936899d5d
juan666 <Juanpabloflorezcruz@gmail.com>
2022-04-02 17:19:42 -0500

    cambios activity_descripcion

--- app/src/main/java/com/app_investigarte/ListadoArtefactos/RecyVWAdapter.java
+++ app/src/main/java/com/app_investigarte/ListadoArtefactos/RecyVWAdapter.java
@@ -1,5 +1,6 @@
 package com.app_investigarte.ListadoArtefactos;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
