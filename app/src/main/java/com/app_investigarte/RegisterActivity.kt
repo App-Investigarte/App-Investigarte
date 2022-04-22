3261cb5f39e2b4f9c9db98f93c85bb52aabebcb8
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-04-21 20:32:09 -0500

    implementacion de login con Google y feirebase, Actualizacion de kotlin de 1.6.10 a 1.6.21, actualizacion de la variables por el metodo de viewBinding

--- app/src/main/java/com/app_investigarte/RegisterActivity.kt
+++ app/src/main/java/com/app_investigarte/RegisterActivity.kt
@@ -1,31 +1,30 @@
 package com.app_investigarte
 
+import android.annotation.SuppressLint
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.InputType
-import android.text.TextWatcher
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.ArrayAdapter
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.util.PatternsCompat
 import com.app_investigarte.database.DatabaseAccess
+import com.app_investigarte.databinding.ActivityRegisterBinding
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.textfield.TextInputLayout
-import kotlinx.android.synthetic.main.activity_register.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
     var emptyData: Int = 0
 
+    private lateinit var binding: ActivityRegisterBinding
+
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val today = MaterialDatePicker.todayInUtcMilliseconds()
         val datePicker =
@@ -34,7 +33,7 @@ class RegisterActivity : AppCompatActivity() {
                 .setSelection(today)
                 .build()
 
-        btn_date.setOnClickListener { showDatePicker(datePicker) }
+        binding.btnDate.setOnClickListener { showDatePicker(datePicker) }
 
         controlFlujoEdt()
 
@@ -42,9 +41,9 @@ class RegisterActivity : AppCompatActivity() {
             val formatter = SimpleDateFormat("dd/MM/yyyy")
             val calendar = Calendar.getInstance()
             calendar.timeInMillis = (datePicker.selection?.plus(100000000)!!)
-            txt_date.setText(formatter.format(calendar.timeInMillis))
+            binding.txtDate.setText(formatter.format(calendar.timeInMillis))
         }
-        btn_register.setOnClickListener {
+        binding.btnRegister.setOnClickListener {
             emptyData = 0
             validacionCamposVacidos()
             //Registro de usuario
@@ -57,15 +56,17 @@ class RegisterActivity : AppCompatActivity() {
     private fun controlFlujoEdt() {
 
         //ocular el teclado cunado se precione enter
-        edt_name1.setOnKeyListener(View.OnKeyListener { view, keyCode, keyEvent ->
-            if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                if (edt_name1.text?.isNotEmpty()!!) {
-                    view.hideKeyboard()
+        with(binding) {
+            edtName1.setOnKeyListener(View.OnKeyListener { view, keyCode, keyEvent ->
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    if (edtName1.text?.isNotEmpty()!!) {
+                        view.hideKeyboard()
+                    }
+                    return@OnKeyListener true
                 }
-                return@OnKeyListener true
-            }
-            false
-        })
+                false
+            })
+        }
     }
 
     fun View.hideKeyboard() {
@@ -80,59 +81,65 @@ class RegisterActivity : AppCompatActivity() {
 
     fun validacionCamposVacidos() {
 
-        //validacion de la identificacion
-        if (edt_id.text.toString().isEmpty()) {
-            lyt_id.error = getString(R.string.edt_id_error)
-            edt_id.height = 5
-            edt_id.setPaddingRelative(20, 7, 7, -7)
-            emptyData++
-        } else {
-            edt_id.setPaddingRelative(20, 7, 7, 7)
-            edt_id.height = 35
-            lyt_id.error = null
-            lyt_id.isErrorEnabled = false
-        }
+        with(binding) {
+            //validacion de la identificacion
+            if (edtId.text.toString().isEmpty()) {
+                lytId.error = getString(R.string.edt_id_error)
+                edtId.height = 5
+                edtId.setPaddingRelative(20, 7, 7, -7)
+                emptyData++
+            } else {
+                edtId.setPaddingRelative(20, 7, 7, 7)
+                edtId.height = 35
+                lytId.error = null
+                lytId.isErrorEnabled = false
+            }
 
-        //Validaciones del Correo Electronico
-        if (edt_correo.text.toString().isEmpty()) {
-            lyt_correo.error = "Por Favor Ingrese el Correo"
-            edt_correo.height = 5
-            edt_correo.setPaddingRelative(20, 7, 7, -7)
-            emptyData++
+            //Validaciones del Correo Electronico
+            if (edtCorreo.text.toString().isEmpty()) {
+                lytCorreo.error = "Por Favor Ingrese el Correo"
+                edtCorreo.height = 5
+                edtCorreo.setPaddingRelative(20, 7, 7, -7)
+                emptyData++
 
-        } else if (!PatternsCompat.EMAIL_ADDRESS.matcher(edt_correo.text.toString()).matches()) {
-            //se verifica qeuse aya escrito un correo electronico valido
-            lyt_correo.error = "Por Favor Ingrese un Correo valido"
-            edt_correo.height = 5
-            edt_correo.setPaddingRelative(20, 7, 7, -7)
-            emptyData++
-        } else {
-            edt_correo.setPaddingRelative(20, 7, 7, 7)
-            edt_correo.height = 35
-            lyt_correo.error = null
-            lyt_correo.isErrorEnabled = false
-        }
+            } else if (!PatternsCompat.EMAIL_ADDRESS.matcher(edtCorreo.text.toString())
+                    .matches()
+            ) {
+                //se verifica qeuse aya escrito un correo electronico valido
+                lytCorreo.error = "Por Favor Ingrese un Correo valido"
+                edtCorreo.height = 5
+                edtCorreo.setPaddingRelative(20, 7, 7, -7)
+                emptyData++
+            } else {
+                edtCorreo.setPaddingRelative(20, 7, 7, 7)
+                edtCorreo.height = 35
+                lytCorreo.error = null
+                lytCorreo.isErrorEnabled = false
+            }
 
-        //Validaciones del Nombre
-        if (edt_name1.text.toString().isEmpty()) {
-            lyt_name1.error = "Ingrese el Nombre completo"
-            edt_name1.height = 5
-            edt_name1.setPaddingRelative(20, 7, 7, -7)
-            emptyData++
-        } else {
-            edt_name1.setPaddingRelative(20, 7, 7, 7)
-            edt_name1.height = 35
-            lyt_name1.error = null
-            lyt_name1.isErrorEnabled = false
+            //Validaciones del Nombre
+            if (edtName1.text.toString().isEmpty()) {
+                lytName1.error = "Ingrese el Nombre completo"
+                edtName1.height = 5
+                edtName1.setPaddingRelative(20, 7, 7, -7)
+                emptyData++
+            } else {
+                edtName1.setPaddingRelative(20, 7, 7, 7)
+                edtName1.height = 35
+                lytName1.error = null
+                lytName1.isErrorEnabled = false
+            }
         }
     }
 
     fun clearDataEdt() {
-        edt_correo.setText("")
-        edt_name1.setText("")
-        edt_id.setText("")
-        edt_telefono.text?.clear()
-        txt_date.setText("")
+        with(binding) {
+            edtCorreo.setText("")
+            edtName1.setText("")
+            edtId.setText("")
+            edtTelefono.text?.clear()
+            txtDate.setText("")
+        }
     }
 
     fun AlertRegistroexitoso() {
@@ -153,42 +160,43 @@ class RegisterActivity : AppCompatActivity() {
 
     fun registerUser() {
 
-        val id: String = edt_id.text.toString()
-        val email: String = edt_correo.text.toString()
-        val name: String = edt_name1.text.toString()
-        var phone: String = "0"
-        var date: String = " "
-
-        if(edt_telefono.text?.isNotEmpty()!!){
-            phone = edt_telefono.text.toString()
-        }
-
-        if(txt_date.text.isNotEmpty()){
-            date = txt_date.text.toString()
-        }
-
         val databaseAcces: DatabaseAccess = DatabaseAccess.getInstance(this)
         databaseAcces.open()
+        with(binding) {
+            val id: String = edtId.text.toString()
+            val email: String = edtCorreo.text.toString()
+            val name: String = edtName1.text.toString()
+            var phone = "0"
+            var date = " "
 
-        val userExiste: Int = databaseAcces.getUserExistencia(id.toLong())
-        if (userExiste == 0) {
-            val emailexiste: Int = databaseAcces.getUserCorreoExiste(email)
-            if(emailexiste == 0){
-                databaseAcces.addUser(id, email, name, phone, date)
-                clearDataEdt()
-                AlertRegistroexitoso()
-            }else{
-                //el usuario ya existe en la base de datos
-                AlertRegistroUserExiste()
-                lyt_correo.error = getString(R.string.correo_existe)
-                edt_correo.setPaddingRelative(20, 7, 7, -7)
+            if (edtTelefono.text?.isNotEmpty()!!) {
+                phone = edtTelefono.text.toString()
             }
-        } else {
-            //el usuario ya existe en la abse de datos
-            AlertRegistroUserExiste()
-            lyt_id.error = getString(R.string.user_existe)
-            edt_id.setPaddingRelative(20, 7, 7, -7)
 
+            if (txtDate.text.isNotEmpty()) {
+                date = txtDate.text.toString()
+            }
+
+            val userExiste: Int = databaseAcces.getUserExistencia(id.toLong())
+            if (userExiste == 0) {
+                val emailexiste: Int = databaseAcces.getUserCorreoExiste(email)
+                if (emailexiste == 0) {
+                    databaseAcces.addUser(id, email, name, phone, date)
+                    clearDataEdt()
+                    AlertRegistroexitoso()
+                } else {
+                    //el usuario ya existe en la base de datos
+                    AlertRegistroUserExiste()
+                    lytCorreo.error = getString(R.string.correo_existe)
+                    edtCorreo.setPaddingRelative(20, 7, 7, -7)
+                }
+            } else {
+                //el usuario ya existe en la abse de datos
+                AlertRegistroUserExiste()
+                lytId.error = getString(R.string.user_existe)
+                edtId.setPaddingRelative(20, 7, 7, -7)
+
+            }
         }
         databaseAcces.close()
     }
